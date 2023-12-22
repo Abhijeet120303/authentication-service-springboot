f7f1f6543b16e47d2e42ce8199e88fb23b1dbc39
Abhijeet Nirphal <nirphalabhijeet72@gmail.com>
2023-12-22 10:05:06 +0530

    Initial commit

---
+++ src/main/java/org/dnyanyog/services/AddUserService.java
@@ -0,0 +1,22 @@
+package org.dnyanyog.services;
+
+import org.dnyanyog.dto.AddUserRequest;
+import org.dnyanyog.dto.AddUserResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+public class AddUserService {
+	@PostMapping(path="/api/auth/add-user")
+	public AddUserResponse addUser(AddUserRequest addUserRequest) {
+		AddUserResponse response=new AddUserResponse();
+		response.setStatus("success");
+		response.setMessage("User Successfully added");
+		response.setUser_id(addUserRequest.getUser_id());
+		response.setUser_name(addUserRequest.getUser_name());
+		response.setUser_email(addUserRequest.getUser_email());
+		response.setUser_age(addUserRequest.getUser_age());
+		return response;
+	}
+
+}
